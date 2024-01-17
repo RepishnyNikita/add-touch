073daa545d12789e8f21c7dcbcbddab1d388d7a3
RepishnyNikita <armyrepa@maill.ru>
2024-01-17 16:38:04 +0300

    adaptiv-faq-footer

--- js/main.js
+++ js/main.js
@@ -1,34 +1,39 @@
-function accordion (){
-    const toggleIcon = document.querySelector(".accordion__header-toggle");
-    const accordionDesc = document.querySelector(".accordion__desc");
-    const accordion = document.querySelectorAll('[data-name="accordion"]')
+function accordion() {
+  const toggleIcon = document.querySelectorAll(".accordion__header-toggle");
+  const toggleIconOpen = document.querySelector(".accordion__header-toggle__icon")
+  const accordionDesc = document.querySelector(".accordion__desc");
+  const accordion = document.querySelectorAll('[data-name="accordion"]');
 
-
-    accordion.forEach(function(item){
-
-    item.addEventListener('click', function () {
-        this.nextElementSibling.classList.toggle('accordion__desc--hidden')
+  accordion.forEach(function (item) {
+    item.addEventListener("click", function () {
+      this.nextElementSibling.classList.toggle("accordion__desc--hidden");
     });
-})
+
+    // for (let toggle of toggleIcon){
+    //     toggle.addEventListener("click", function (){
+    //         toggleIconOpen.classList.toggle("accordion__header-toggle__icon--open");
+    //         console.log('cl')
+    //     })
+    // }
+
+    
+  });
 }
+accordion();
 
-accordion()
+function navMobile() {
+  const navIcon = document.querySelector(".nav-toggle");
+  const navMobile = document.querySelector(".nav-mobile");
+  const navMobileIcon = document.querySelector(".nav-mobile__icon");
 
-function navMobile(){
-    const navIcon = document.querySelector('.nav-toggle')
-    const navMobile = document.querySelector('.nav-mobile')
-    const navMobileIcon = document.querySelector('.nav-mobile__icon')
+  navIcon.addEventListener("click", function () {
+    navMobile.classList.toggle("open");
 
-    navIcon.addEventListener('click', function () {
-
-        navMobile.classList.toggle('open')
-
-        if (navMobileIcon.getAttribute("src") == "./img/svg/nav.svg"){
-            navMobileIcon.src = "./img/svg/close-nav.svg"}
-        else{
-            navMobileIcon.src = "./img/svg/nav.svg"}
-    })  
+    if (navMobileIcon.getAttribute("src") == "./img/svg/nav.svg") {
+      navMobileIcon.src = "./img/svg/close-nav.svg";
+    } else {
+      navMobileIcon.src = "./img/svg/nav.svg";
+    }
+  });
 }
-
-navMobile()
-
+navMobile();
