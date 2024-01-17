c69460c9d5977fa3dfc46f2a9a9506b707e09129
RepishnyNikita <armyrepa@maill.ru>
2024-01-17 14:11:59 +0300

    mobile-adaptive-price

--- js/main.js
+++ js/main.js
@@ -1,12 +1,34 @@
-const toggleIcon = document.querySelector(".accordion__header-toggle");
-const accordionDesc = document.querySelector(".accordion__desc");
-const accordion = document.querySelectorAll('[data-name="accordion"]')
+function accordion (){
+    const toggleIcon = document.querySelector(".accordion__header-toggle");
+    const accordionDesc = document.querySelector(".accordion__desc");
+    const accordion = document.querySelectorAll('[data-name="accordion"]')
 
 
-accordion.forEach(function(item){
+    accordion.forEach(function(item){
 
     item.addEventListener('click', function () {
         this.nextElementSibling.classList.toggle('accordion__desc--hidden')
     });
-
 })
+}
+
+accordion()
+
+function navMobile(){
+    const navIcon = document.querySelector('.nav-toggle')
+    const navMobile = document.querySelector('.nav-mobile')
+    const navMobileIcon = document.querySelector('.nav-mobile__icon')
+
+    navIcon.addEventListener('click', function () {
+
+        navMobile.classList.toggle('open')
+
+        if (navMobileIcon.getAttribute("src") == "./img/svg/nav.svg"){
+            navMobileIcon.src = "./img/svg/close-nav.svg"}
+        else{
+            navMobileIcon.src = "./img/svg/nav.svg"}
+    })  
+}
+
+navMobile()
+
