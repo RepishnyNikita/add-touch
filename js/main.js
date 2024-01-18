33f1d6274c781c8a8064bbd94c98580b2213ac61
RepishnyNikita <armyrepa@maill.ru>
2024-01-18 13:34:18 +0300

    fix-accord

--- js/main.js
+++ js/main.js
@@ -1,22 +1,11 @@
 function accordion() {
-  const toggleIcon = document.querySelectorAll(".accordion__header-toggle");
-  const toggleIconOpen = document.querySelector(".accordion__header-toggle__icon")
-  const accordionDesc = document.querySelector(".accordion__desc");
   const accordion = document.querySelectorAll('[data-name="accordion"]');
 
   accordion.forEach(function (item) {
     item.addEventListener("click", function () {
       this.nextElementSibling.classList.toggle("accordion__desc--hidden");
+      this.lastElementChild.firstElementChild.classList.toggle('accordion__header-button__icon--open')
     });
-
-    // for (let toggle of toggleIcon){
-    //     toggle.addEventListener("click", function (){
-    //         toggleIconOpen.classList.toggle("accordion__header-toggle__icon--open");
-    //         console.log('cl')
-    //     })
-    // }
-
-    
   });
 }
 accordion();
@@ -27,7 +16,9 @@ function navMobile() {
   const navMobileIcon = document.querySelector(".nav-mobile__icon");
 
   navIcon.addEventListener("click", function () {
+    this.classList.toggle("nav-toggle--open")
     navMobile.classList.toggle("open");
+
 
     if (navMobileIcon.getAttribute("src") == "./img/svg/nav.svg") {
       navMobileIcon.src = "./img/svg/close-nav.svg";
